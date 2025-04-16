4e9e2bf54a7112d9b0043488fac07a7ba398ccbb
lguerrini <luca.guerrini@gmail.com>
2025-04-16 12:46:50 +0200

    poca roba

--- f1.py
+++ f1.py
@@ -1,3 +1,25 @@
 #######
-print("qwe")
-print("qwe")
+print("qwe")   
+g=123 
+print("cdfbdfbdqwe")
+class Computer:
+    
+    def __init__(self):
+        self.__maxprice = 900
+
+    def sell(self):
+        print(f"Selling Price: {self.__maxprice} {g}")
+
+    def setMaxPrice(self, price):
+        self.__maxprice = price
+
+c = Computer()
+c.sell()
+
+# change the price
+c.__maxprice = 1000
+c.sell()
+
+# using setter function
+c.setMaxPrice(1000)
+c.sell()
